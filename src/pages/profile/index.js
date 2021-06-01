4c80836381042fa11051a6987e270ed55e5b60ca
Vinicius Gusmao <vinicius-og@hotmail.com>
2021-06-01 16:31:23 -0300

    Added translation files

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -5,22 +5,25 @@ import AppBar from "component/statefull/AppBar";
 import IconButton from "component/ui/IconButton";
 import FooterApp from "component/layout/FooterApp";
 import Divider from "component/ui/Divider";
+import useTranslation from "hooks/useTranslation";
 
-function Profile() {
+function Profile(props) {
+  const { t } = useTranslation(props.lang, "profile");
+
   return (
     <Container extraStyle={{ padding: 0 }}>
       <FlexBox extraStyle={{ padding: 0 }}>
-        <AppBar title="Profile" />
+        <AppBar title={t?.title} />
         <div style={{ display: "flex", flexDirection: "column" }}>
           <IconButton
-            title="Editar Perfil"
+            title={t?.textEditButton}
             icon="EditIcon"
             variantTitle="p"
             fontSizeIcon="1.8em"
           />
           <Divider marginBottom={20} />
           <IconButton
-            title="Subir foto"
+            title={t?.textPhotoButton}
             icon="PhotoCameraIcon"
             variantTitle="p"
             fontSizeIcon="1.8em"
