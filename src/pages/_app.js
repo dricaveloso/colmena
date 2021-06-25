cab8d8932f837356e088ef5e297ec71e8e9ab291
Vinicius Gusmao <vinicius-og@hotmail.com>
2021-06-25 08:23:46 -0300

    Fix pages without css when reload it

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,28 @@
-import React, { useEffect } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import Head from "next/head";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { appWithTranslation } from "next-i18next";
 import GlobalLayout from "component/layout/GlobalLayout";
 import { NotificationContextProvider } from "store/notification-context";
 import { UserContextProvider } from "store/user-context";
-import { CacheProvider } from "@emotion/react";
-import createCache from "@emotion/cache";
-import PropTypes from "prop-types";
+import { appWithTranslation } from "next-i18next";
 import theme from "styles/theme";
 import "styles/globals.css";
-
-const cache = createCache({ key: "css" });
-cache.compat = true;
 
 function MyApp(props) {
   const { Component, pageProps } = props;
 
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
-    <CacheProvider value={cache}>
+    <React.Fragment>
       <Head>
         <title>MAIA - Create, Collaborate and Share </title>
         <meta
@@ -37,9 +40,10 @@ function MyApp(props) {
           </UserContextProvider>
         </NotificationContextProvider>
       </ThemeProvider>
-    </CacheProvider>
+    </React.Fragment>
   );
 }
+
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
   pageProps: PropTypes.object.isRequired,
--- src/pages/teste/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import TextField from "@material-ui/core/TextField";
-
-function Teste() {
-  return <TextField id="teste2" variant="outlined" label="Teste" />;
-}
-
-export default Teste;
