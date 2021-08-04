9f681a8082eb02d23eebcd4e3ff194d8baa2eb61
Vinicius Gusmao <vinicius-og@hotmail.com>
2021-08-04 17:23:40 -0300

    Bug feature/#67

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,5 +3,5 @@ export default {
   APP_DESCRIPTION: "Open collaborative tools to create and share",
   LOCALES: { en: "en", es: "es", fr: "fr" },
   DEFAULT_LANGUAGE: "en",
-  TOKEN_EXPIRE_SECONDS: 60 * 5,
+  TOKEN_EXPIRE_SECONDS: 5,
 };
