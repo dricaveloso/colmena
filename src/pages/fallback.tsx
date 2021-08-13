8cea97646b7558fb924ab8db8c369b2ae8acab85
Vinicius Gusmao <vinicius-og@hotmail.com>
2021-08-13 09:15:16 -0300

    Fallback screen updated

--- src/pages/fallback.tsx
+++ src/pages/fallback.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import FullCenterContainer from "@/components/ui/FullCenterContainer";
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+// import { useTranslation } from "next-i18next";
+// import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import ResourceUnavailable from "@/components/ui/ResourceUnavailable";
-import { GetStaticProps } from "next";
-import { I18nInterface } from "@/interfaces/index";
+// import { GetStaticProps } from "next";
+// import { I18nInterface } from "@/interfaces/index";
 
-export const getStaticProps: GetStaticProps = async ({ locale }: I18nInterface) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ["common"])),
-  },
-});
+// export const getStaticProps: GetStaticProps = async ({ locale }: I18nInterface) => ({
+//   props: {
+//     ...(await serverSideTranslations(locale, ["common"])),
+//   },
+// });
 
 function Fallback() {
-  const { t } = useTranslation("common");
+  // const { t } = useTranslation("common");
 
   return (
     <FullCenterContainer>
-      <ResourceUnavailable icon="wifi_off" title={t("messageOffline")} />
+      <ResourceUnavailable icon="wifi_off" />
     </FullCenterContainer>
   );
 }
