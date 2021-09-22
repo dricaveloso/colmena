bf209cb7ddc60dc28a2a021b05f5f8f5f56bc0fd
Nilson  Rocha <nilson.f.rocha@gmail.com>
2021-09-22 11:20:41 +0000

    Update  variable index.ts

--- src/services/ocs/index.ts
+++ src/services/ocs/index.ts
@@ -4,7 +4,7 @@ import { initializeStore } from "@/store/index";
 const ocs = () => {
   const { userToken } = initializeStore({}).getState().user.user;
   const api = axios.create({
-    baseURL: `https://claudio.colmena.network/ocs/v2.php/cloud`,
+    baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/ocs/v2.php/cloud`,
     headers: {
       "OCS-APIRequest": true,
       Authorization: `Bearer ${userToken}`,
