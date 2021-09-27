d1083b38cffdb539cfe38005342793aadb496370
= <nilson.f.rocha@gmail.com>
2021-09-27 14:50:05 -0300

    app password

--- src/services/ocs/index.ts
+++ src/services/ocs/index.ts
@@ -5,9 +5,12 @@ import { initializeStore } from "@/store/index";
 const ocs = () => {
   const { userToken } = initializeStore({}).getState().user.user;
   console.log(userToken);
-
   const api = axios.create({
     baseURL: `${process.env.NEXT_PUBLIC_API_BASE_URL}/ocs/v2.php/cloud`,
+    // auth: {
+    //   username: "nilson.f.rocha@gmail.com",
+    //   password: "3aowM-TSook-GBtTn-GbAYP-weYkT",
+    // },
     headers: {
       "OCS-APIRequest": true,
       Authorization: `Bearer ${userToken}`,
