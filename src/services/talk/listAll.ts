91a0e576f2263ee0a8124221377e75a0d2694bb5
= <nilson.f.rocha@gmail.com>
2021-10-14 20:41:50 -0300

    create all and especific chat

--- src/services/talk/listAll.ts
+++ src/services/talk/listAll.ts
@@ -1,19 +1,27 @@
 import talk from "@/services/talk";
 
-export default function listAllTalk() {
-  try {
-    return talk()
-      .get(`/room`)
-      .then((response) => {
-        // handle success
-        console.log(response);
-      })
-      .catch((error) => {
-        // handle error
-        console.log(error);
-      });
-  } catch (err) {
-    console.log(err.response);
-    return false;
-  }
+export function listAllTalk() {
+  return talk()
+    .get(`/room`)
+    .then((response) => {
+      // handle success
+      console.log(response);
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    });
+}
+
+export function listEspecificChat(token: string) {
+  return talk()
+    .get(`/room/${token}`)
+    .then((response) => {
+      // handle success
+      console.log(response);
+    })
+    .catch((error) => {
+      // handle error
+      console.log(error);
+    });
 }
