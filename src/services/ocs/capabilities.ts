199c1d5c6f21183010daa68cf5cff7a279d8f1cb
= <nilson.f.rocha@gmail.com>
2021-10-14 08:31:35 -0300

    remove try catch in service capabilities

--- src/services/ocs/capabilities.ts
+++ src/services/ocs/capabilities.ts
@@ -3,19 +3,12 @@ import ocs from "@/services/ocs";
 // import ocs from ".";
 
 export default function listAllCapabilities() {
-  try {
-    return ocs()
-      .get(`/capabilities`)
-      .then((response) => {
-        // handle success
-        console.log(response);
-      })
-      .catch((error) => {
-        // handle error
-        console.log(error);
-      });
-  } catch (err) {
-    console.log(err.response);
-    return false;
-  }
+  return ocs()
+    .get(`/capabilities`)
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
