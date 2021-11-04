6e0adfe6e0576b6fbbeb99f64f12e69d0051bbd7
= <nilson.f.rocha@gmail.com>
2021-11-04 17:07:50 -0300

    version 4

--- src/services/talk/room.ts
+++ src/services/talk/room.ts
@@ -17,46 +17,46 @@ import talkInstance from "@/services/talk";
 const responseFormat = "?format=json";
 
 export function getUsersConversations(options?: {}): RoomListInterface {
-  return useTalkFetch("v3")(`/room${responseFormat}`, {}, options);
+  return useTalkFetch("v4")(`/room${responseFormat}`, {}, options);
 }
 
 export function getOpenConversations(): RoomListInterface {
-  return useTalkFetch("v3")(`/listed-room${responseFormat}`);
+  return useTalkFetch("v4")(`/listed-room${responseFormat}`);
 }
 
 export function getSingleConversation(token: string): RoomInterface {
-  return useTalkFetch("v3")(`/room/${token + responseFormat}`);
+  return useTalkFetch("v4")(`/room/${token + responseFormat}`);
 }
 
 export function getRoomParticipants(token: string, options?: {}): RoomParticipantsInterface {
-  return useTalkFetch("v3")(`/room/${token}/participants${responseFormat}`, {}, options);
+  return useTalkFetch("v4")(`/room/${token}/participants${responseFormat}`, {}, options);
 }
 
 export function setReadOnlyConversation(
   token: string,
   status: number,
 ): Promise<ReadOnlyRoomInterface> {
-  return talkInstance("v3").put(`room/${token}/read-only${responseFormat}`, {
+  return talkInstance("v4").put(`room/${token}/read-only${responseFormat}`, {
     status,
   });
 }
 
 export function createNewConversation(roomName: string): Promise<RoomCreateInterface> {
-  return talkInstance("v3").post(`room${responseFormat}`, {
+  return talkInstance("v4").post(`room${responseFormat}`, {
     roomType: 2,
     roomName,
   });
 }
 
 export function deleteConversation(token: string): Promise<DeleteConversationInterface> {
-  return talkInstance("v3").delete(`room/${token}/participants/self${responseFormat}`);
+  return talkInstance("v4").delete(`room/${token}/participants/self${responseFormat}`);
 }
 
 export function addDescriptionConversation(
   token: string,
   description: string,
 ): Promise<ParticipantCreateInterface> {
-  return talkInstance("v3").put(`room/${token}/description${responseFormat}`, {
+  return talkInstance("v4").put(`room/${token}/description${responseFormat}`, {
     description,
   });
 }
@@ -65,7 +65,7 @@ export function addParticipantToConversation(
   token: string,
   newParticipant: string,
 ): Promise<ParticipantCreateInterface> {
-  return talkInstance("v3").post(`room/${token}/participants${responseFormat}`, {
+  return talkInstance("v4").post(`room/${token}/participants${responseFormat}`, {
     newParticipant,
     source: "users",
   });
