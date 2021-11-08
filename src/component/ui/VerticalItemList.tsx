aa9259dbd6aec7039492c862affa1b49deaf176d
Patrick Oliveira <contato@widedevelop.com>
2021-11-08 08:30:26 -0300

    add library context menu (view only)

--- src/component/ui/VerticalItemList.tsx
+++ src/component/ui/VerticalItemList.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Box from "@material-ui/core/Box";
@@ -11,6 +11,8 @@ import { useRouter } from "next/router";
 import { FileIcon } from "react-file-icon";
 import theme from "@/styles/theme";
 import { makeStyles } from "@material-ui/core";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles(() => ({
   card: {
@@ -52,11 +54,25 @@ const VerticalItemList = ({
   const classes = useStyles();
   const router = useRouter();
 
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleOpenContextMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseContextMenu = () => {
+    setAnchorEl(null);
+  };
+
   const handleClick = () => {
     if (type === "directory" && router.query.path !== filename) {
       router.push(`/library/${filename}`);
     }
   };
+
+  // const handleDeleteItem = () => {
+
+  // }
 
   return (
     <Box className={classes.card}>
@@ -100,8 +116,27 @@ const VerticalItemList = ({
           color="#9A9A9A"
           style={{ padding: 0, margin: 0, minWidth: 30 }}
           fontSizeIcon="small"
+          handleClick={handleOpenContextMenu}
         />
       </Box>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleCloseContextMenu}
+      >
+        {type === "file" && <MenuItem onClick={handleCloseContextMenu}>Edit</MenuItem>}
+        <MenuItem onClick={handleCloseContextMenu}>Copy</MenuItem>
+        <MenuItem onClick={handleCloseContextMenu}>Move</MenuItem>
+        <MenuItem onClick={handleCloseContextMenu}>Duplicate</MenuItem>
+        <MenuItem onClick={handleCloseContextMenu}>Download</MenuItem>
+        <MenuItem onClick={handleCloseContextMenu}>Rename</MenuItem>
+        <MenuItem onClick={handleCloseContextMenu}>Details</MenuItem>
+        <MenuItem onClick={handleCloseContextMenu}>Synchronize</MenuItem>
+        <MenuItem onClick={handleCloseContextMenu}>Publish</MenuItem>
+        <MenuItem onClick={handleCloseContextMenu}>Delete</MenuItem>
+      </Menu>
     </Box>
   );
 };
