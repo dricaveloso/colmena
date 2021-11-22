e786b8afd8649ce331836d4554112caaa5497a10
= <nilson.f.rocha@gmail.com>
2021-11-22 13:40:42 -0300

    service all tags

--- src/pages/tags/index.tsx
+++ src/pages/tags/index.tsx
@@ -1,59 +1,57 @@
-// import { useSelector } from "react-redux";
-// import { PropsUserSelector } from "../../types";
-import capabilities from "@/services/ocs/capabilities";
+// // import capabilities from "@/services/ocs/capabilities";
+// // import axios from "axios";
+// // import LayoutApp from "@/components/statefull/LayoutApp";
+// // import { JustifyContentEnum } from "@/enums/*";
+// // import FlexBox from "@/components/ui/FlexBox";
 
-// import VerticalListWebDav from "@/components/ui/VerticalListWebDav";
-import LayoutApp from "@/components/statefull/LayoutApp";
-import { JustifyContentEnum } from "@/enums/*";
-import FlexBox from "@/components/ui/FlexBox";
+// // import {} from "../../services/tags/tags";
 
-import { listAllTags } from "../../services/tags/tags";
-
-export default function OCS() {
-  // const userRdx = useSelector((state: { user: PropsUserSelector }) => state.user);
-  // const [data, setData] = useState([]);
-  // const directory = "";
-  // async function ResetPass() {
-  //   try {
-  //     const resetPassU = await resetPass("nil", "rocha123");
-  //     console.log(resetPassU);
-  //   } catch (e) {
-  //     console.log("error", e);
-  //   }
-  // }
-  async function ListCababilities() {
-    try {
-      const listC = await capabilities();
-      console.log(listC);
-    } catch (e) {
-      console.log("error", e);
-    }
-  }
-  async function ListTags() {
-    try {
-      const listT = await listAllTags();
-      console.log(listT);
-    } catch (e) {
-      console.log("error", e);
-    }
-  }
-  return (
-    <LayoutApp title="title">
-      <FlexBox justifyContent={JustifyContentEnum.FLEXSTART} extraStyle={{ padding: 0 }}>
-        <div style={{ width: "100vw", margin: 5 }}>
-          <FlexBox>
-            <div>
-              <button type="button" onClick={ListCababilities}>
-                Listar Capabilities
-              </button>
-              <button type="button" onClick={ListTags}>
-                Listar Tags
-              </button>
-            </div>
-          </FlexBox>
-          {/* <VerticalListWebDav data={data} /> */}
-        </div>
-      </FlexBox>
-    </LayoutApp>
-  );
-}
+// export default function OCS() {
+//   // const userRdx = useSelector((state: { user: PropsUserSelector }) => state.user);
+//   // const [data, setData] = useState([]);
+//   // const directory = "";
+//   // async function ResetPass() {
+//   //   try {
+//   //     const resetPassU = await resetPass("nil", "rocha123");
+//   //     console.log(resetPassU);
+//   //   } catch (e) {
+//   //     console.log("error", e);
+//   //   }
+//   // }
+//   //   async function ListCababilities() {
+//   //     try {
+//   //       const listC = await capabilities();
+//   //       console.log(listC);
+//   //     } catch (e) {
+//   //       console.log("error", e);
+//   //     }
+//   //   }
+//   //   async function ListTags() {
+//   //     axios(config)
+//   //       .then((response) => {
+//   //         console.log(JSON.stringify(response.data));
+//   //       })
+//   //       .catch((error) => {
+//   //         console.log(error);
+//   //       });
+//   //   }
+//   //   return (
+//   //     <LayoutApp title="title">
+//   //       <FlexBox justifyContent={JustifyContentEnum.FLEXSTART} extraStyle={{ padding: 0 }}>
+//   //         <div style={{ width: "100vw", margin: 5 }}>
+//   //           <FlexBox>
+//   //             <div>
+//   //               <button type="button" onClick={ListCababilities}>
+//   //                 Listar Capabilities
+//   //               </button>
+//   //               <button type="button" onClick={ListTags}>
+//   //                 Listar Tags
+//   //               </button>
+//   //             </div>
+//   //           </FlexBox>
+//   //           {/* <VerticalListWebDav data={data} /> */}
+//   //         </div>
+//   //       </FlexBox>
+//   //     </LayoutApp>
+//   //   );
+// }
--- src/services/tags/tags.ts
+++ src/services/tags/tags.ts
@@ -1,10 +1,31 @@
-import { ListAllInterface } from "@/interfaces/tags";
-import tags from "@/services/tags";
+// import { ListAllInterface } from "@/interfaces/tags";
+// import tags from "@/services/tags";
+// import axios from "axios";
+// import { initializeStore } from "@/store/index";
 
-export function listAllTags(): ListAllInterface {
-  return tags().PROPFIND("/");
-}
-
-// export function createTag(): CreateTagInterface {
-//   return tags.put
+// export function listAllTags(): ListAllInterface {
+//   const data =
+//     "<?xml version=\"1.0\" encoding=\"utf-8\" ?>\n<a:propfind xmlns:a=\"DAV:\" xmlns:oc=\"http://owncloud.org/ns\">\n  <a:prop>\n    <!-- Retrieve the display-name, user-visible, and user-assignable properties -->\n    <oc:display-name/>\n    <oc:user-visible/>\n    <oc:user-assignable/>\n    <oc:id/>\n  </a:prop>\n</a:propfind>";
+//   const { password, id: username } = initializeStore({}).getState().user.user;
+//   const config = {
+//     method: "propfind",
+//     // Authorization: {
+//     //   username,
+//     //   password,
+//     // },
+//     url: `${process.env.NEXT_PUBLIC_API_BASE_URL}remote.php/dav/systemtags`,
+//     headers: {
+//       "OCS-APIRequest": "true",
+//       "Content-Type": "application/xml",
+//       Authorization: "Basic YWRtaW46MzU3N3VtVFVTRDJxdmIz",
+//     },
+//     data,
+//   };
+//   axios(config)
+//     .then((response) => {
+//       console.log(JSON.stringify(response.data));
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
 // }
