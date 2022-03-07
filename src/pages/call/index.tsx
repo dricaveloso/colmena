8a355cc7156b258c0ef0001c5ac38b817bb87f7b
Marco Brito <marcobritoh@gmail.com>
2022-03-07 08:16:09 -0300

    module audio conference

--- src/pages/call/index.tsx
+++ src/pages/call/index.tsx
@@ -1,4 +1,8 @@
+/* eslint-disable react/jsx-boolean-value */
+/* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
 import React from "react";
 import FlexBox from "@/components/ui/FlexBox";
 import LayoutApp from "@/components/statefull/LayoutApp";
@@ -11,8 +15,10 @@ import { GetStaticProps } from "next";
 import { I18nInterface } from "@/interfaces/index";
 import { AlignItemsEnum, JustifyContentEnum } from "@/enums/index";
 import GoLive from "@/components/pages/call/GoLive";
+import dynamic from "next/dynamic";
 import serverSideTranslations from "@/extensions/next-i18next/serverSideTranslations";
-
+// @ts-ignore
+const WebRTCClient = dynamic(() => import("freepbx-react-webrtc"), { ssr: false });
 export const getStaticProps: GetStaticProps = async ({ locale }: I18nInterface) => ({
   props: {
     ...(await serverSideTranslations(locale, ["call", "recording"])),
@@ -30,6 +36,38 @@ function Call() {
         <ShareLinkComponent url="https://dev.maia.press/jghd-asde-erty" />
         <RecordUsers />
         <Timer />
+        <div className="App">
+          <video width="25%" id="localVideo" autoPlay playsInline muted></video>
+
+          <video width="50%" id="remoteVideo" autoPlay playsInline></video>
+
+          <WebRTCClient
+          // @ts-ignore
+
+            video={true}
+            autoRegister
+            autoConnect={false}
+            sipDomain=""
+            sipServer=""
+            sipUser=""
+            destination=""
+            metaData={{}}
+            alertVideoUrl="alertUrl"
+            ringbackVideoUrl="ringbackUrl"
+            hideConnectionStatus={false}
+            hideControls={false}
+            autoAnswer={false}
+            hangupCallNow={false}
+            traceSip={false}
+            callLabel="Custom call label"
+            remoteVideo="remoteVideoElementId"
+            localVideo="localVideoElementId"
+            onConnected={() => {}}
+            onConnecting={() => {}}
+            onDisconnected={() => {}}
+            onHangup={() => {}}
+          />
+        </div>
       </FlexBox>
     </LayoutApp>
   );
