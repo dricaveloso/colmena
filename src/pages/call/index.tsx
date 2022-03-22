06c28147de8b753b9683c1f365332fee28bc1567
Marco Brito <marcobritoh@gmail.com>
2022-03-22 08:09:47 -0300

    include getUserMedia and show call webtrc

--- src/pages/call/index.tsx
+++ src/pages/call/index.tsx
@@ -1,10 +1,14 @@
+/* eslint-disable no-multi-assign */
+/* eslint-disable prefer-destructuring */
+/* eslint-disable react/button-has-type */
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable react/jsx-boolean-value */
 /* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/ban-ts-comment */
-
-import React from "react";
+// @ts-nocheck
+import React, { useEffect } from "react";
 import FlexBox from "@/components/ui/FlexBox";
 import LayoutApp from "@/components/statefull/LayoutApp";
 import RecordUsers from "@/components/pages/call/RecordUsers";
@@ -16,9 +20,7 @@ import { GetStaticProps } from "next";
 import { I18nInterface } from "@/interfaces/index";
 import { AlignItemsEnum, JustifyContentEnum } from "@/enums/index";
 import GoLive from "@/components/pages/call/GoLive";
-// import dynamic from "next/dynamic";
 // @ts-ignore
-import SimpleWebRTC from "simplewebrtc";
 import serverSideTranslations from "@/extensions/next-i18next/serverSideTranslations";
 
 export const getStaticProps: GetStaticProps = async ({ locale }: I18nInterface) => ({
@@ -29,25 +31,69 @@ export const getStaticProps: GetStaticProps = async ({ locale }: I18nInterface) 
 
 function Call() {
   // const router = useRouter();
-  const { t } = useTranslation("call");
-  const webrtc = new SimpleWebRTC({
-    // the id/element dom element that will hold "our" video
-    localVideoEl: "localVideo",
-    // the id/element dom element that will hold remote videos
-    remoteVideosEl: "remoteVideos",
-    media: {
-      video: true,
-      audio: true,
-    },
-    // immediately ask for camera access
-    autoRequestMedia: true,
-  });
-  webrtc.on("readyToCall", () => {
-    // you can name it anything
-    webrtc.joinRoom("your awesome room name");
-  });
 
-  webrtc.startLocalVideo();
+  const { t } = useTranslation("call");
+
+  useEffect(() => {
+    import("peerjs").then(({ default: Peer }) => {
+      const peer = new Peer("a");
+      const conn = peer.connect("another-peers-id");
+      conn.on("open", () => {
+        conn.send("hi!");
+        console.log("hiii");
+      });
+      peer.on("connection", (conn) => {
+        conn.on("data", (data) => {
+          // Will print 'hi!'
+          console.log(data);
+        });
+        conn.on("open", () => {
+          conn.send("hello!");
+        });
+      });
+      const getUserMedia =
+        navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+      getUserMedia(
+        { video: true, audio: true },
+        (stream) => {
+          const call = peer.call("a", stream);
+          call.on("stream", (remoteStream) => {
+            // Show stream in some video/canvas element.
+            const video = document.querySelector("localVideo");
+            console.log(video);
+            const videoTracks = remoteStream.getVideoTracks();
+            console.log(`Using video device: ${videoTracks[0].label}`);
+            window.stream = remoteStream; // make variable available to browser console
+            video.srcObject = remoteStream;
+          });
+        },
+        (err) => {
+          console.log("Failed to get local stream", err);
+        },
+      );
+    });
+  }, []);
+
+  async function init() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: true,
+      });
+      handleSuccess(stream);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function handleSuccess(stream) {
+    const video = document.querySelector("video");
+    const videoTracks = stream.getVideoTracks();
+    console.log("Got stream with constraints:");
+    console.log(`Using video device: ${videoTracks[0].label}`);
+    window.stream = stream; // make variable available to browser console
+    video.srcObject = stream;
+  }
 
   return (
     <LayoutApp title={t("title")} back>
@@ -56,10 +102,10 @@ function Call() {
         <ShareLinkComponent url="https://dev.maia.press/jghd-asde-erty" />
         <RecordUsers />
         <Timer />
-        <>
-          <video id="localVideo" height={150}></video>
-          <div id="remoteVideos"></div>
-        </>
+        <video id="localVideo" autoPlay playsInline></video>
+        <button onClick={() => init()} type="button">
+          show Video Streaming
+        </button>
       </FlexBox>
     </LayoutApp>
   );
