bedaa1c05934f6c932dbc48ebe240b68e696c77c
= <nilson.f.rocha@gmail.com>
2022-03-28 18:25:32 -0300

    add env Always

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CI_BUILD_REF_NAME=k8deploy
-export CI_REGISTRY_IMAGE=https://colmena.network:5000/maia/frontend
+export CI_REGISTRY_IMAGE=maia.press:5050/maia/frontend
 
 # export $KUBE_CERTIFICATE
 export CI_COMMIT_REF_SLUG=k8deploy
@@ -13,8 +13,10 @@ export HOST_URL=${CI_COMMIT_REF_SLUG}.colmena.network
 # echo "host_url: " $HOST_URL
 export KUBE_NAMESPACE=frontend-7-testk8
 # echo "namespace: " $KUBE_NAMESPACE
-
+export CI_PROJECT_PATH_SLUG=maia-frontend
 export CI_COMMIT_BRANCH=k8deploy
+
+export TAG_TEST=develop
 
 # app.gitlab.com/app: maia-frontend #__CI_PROJECT_PATH_SLUG__
 # app.gitlab.com/env: testk8 # __CI_ENVIRONMENT_SLUG__
@@ -22,24 +24,37 @@ export CI_COMMIT_BRANCH=k8deploy
 # --set image.tag=$CI_COMMIT_BRANCH \
 # --set image.repository=$CI_REGISTRY_IMAGE \
 
-# --set certificate.name=$CI_COMMIT_REF_SLUG \
+# --set image.tag=$CI_COMMIT_BRANCH \
+# --set image.repository=$CI_REGISTRY_IMAGE \
+#    
+# --set serviceAccount.annotations."app\.gilab\.com\/env"=$CI_ENVIRONMENT_SLUG \
+# --set serviceAccount.annotations."app\.gilab\.com\/app"=$CI_PROJECT_PATH_SLUG \
 # --set certificate.issuer_name=letsencrypt-$CI_COMMIT_REF_SLUG \
+# --set certificate.name=$HOST_URL \
 # --set certificate.email=support@colmena.network \
 # --set ingress.tls[0].secretName=$HOST_URL,ingress.tls[0].hosts=$HOST_URL \
+# --set service.port=80,service.type=ClusteIP \
+
+# --set ingress.enabled=True
+# --set ingress.tls[0].hosts[0]=app.example.com
+# --set ingress.tls[0].secretName=example-tls
+# --set ingress.rules[0].host=app.example.com
+
+
 # --set ingress.hosts[0].host=$HOST_URL,ingress.hosts[1].paths[0].path=/,ingress.hosts[1].paths[0].pathType=ImplementationSpecific \
 
 
-
-helm upgrade --install --debug $CI_COMMIT_REF_SLUG chart \
---set image.tag=$CI_COMMIT_BRANCH \
---set image.repository=$CI_REGISTRY_IMAGE \
--f ./chart/env-values.yaml \
--n $KUBE_NAMESPACE \
---set serviceAccount.annotations."app\.gilab\.com\/env"=testk8 \
---set serviceAccount.annotations."app\.gilab\.com\/app"=maia-frontend \
---set certificate.issuer_name=letsencrypt-$CI_COMMIT_REF_SLUG \
---set certificate.name=$HOST_URL \
---set certificate.email=support@colmena.network \
---set ingress.tls[0].secretName=$HOST_URL,ingress.tls[0].hosts=$HOST_URL \
---set service.port=80,service.type=ClusteIP \
---set ingress.hosts[0].host=$HOST_URL,ingress.hosts[1].paths[0].path=/,ingress.hosts[1].paths[0].pathType=ImplementationSpecific \
+helm upgrade --install --debug $CI_COMMIT_REF_SLUG colmena -n $KUBE_NAMESPACE \
+    --set image.tag=$TAG_TEST \
+    --set image.repository=$CI_REGISTRY_IMAGE \
+    --set serviceAccount.annotations."app\.gilab\.com\/env"=$CI_ENVIRONMENT_SLUG \
+    --set serviceAccount.annotations."app\.gilab\.com\/app"=$CI_PROJECT_PATH_SLUG \
+    --set certificate.issuer_name=letsencrypt-$CI_COMMIT_REF_SLUG \
+    --set certificate.name=$HOST_URL \
+    --set certificate.email=support@colmena.network \
+    --set ingress.enabled=true \
+    --set ingress.hosts[0].host=$HOST_URL \
+    --set ingress.hosts[0].paths[0].path=/ \
+    --set ingress.hosts[0].paths[0].pathType=ImplementationSpecific \
+    --set ingress.tls[0].hosts[0]=$HOST_URL \
+    --set ingress.tls[0].secretName=$HOST_URL \
